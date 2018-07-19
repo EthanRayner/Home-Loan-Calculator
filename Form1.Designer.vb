68ec14ff3d82e47cdd2fc68b1c301e4803a2c928
EthanRayner <41351673+EthanRayner@users.noreply.github.com>
2018-07-20 09:58:20 +1000

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.txtNewHome = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtOldHome = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMortgage = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtStampDuty = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDeposit = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCommission = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnSubmit)
+        Me.GroupBox1.Controls.Add(Me.txtNewHome)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtOldHome)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtMortgage)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 18)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(263, 298)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Inputs"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(16, 215)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(217, 57)
+        Me.btnSubmit.TabIndex = 6
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'txtNewHome
+        '
+        Me.txtNewHome.Location = New System.Drawing.Point(131, 144)
+        Me.txtNewHome.Name = "txtNewHome"
+        Me.txtNewHome.Size = New System.Drawing.Size(126, 20)
+        Me.txtNewHome.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 147)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Price of New Home:"
+        '
+        'txtOldHome
+        '
+        Me.txtOldHome.Location = New System.Drawing.Point(131, 89)
+        Me.txtOldHome.Name = "txtOldHome"
+        Me.txtOldHome.Size = New System.Drawing.Size(126, 20)
+        Me.txtOldHome.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Price of Old Home:"
+        '
+        'txtMortgage
+        '
+        Me.txtMortgage.Location = New System.Drawing.Point(131, 35)
+        Me.txtMortgage.Name = "txtMortgage"
+        Me.txtMortgage.Size = New System.Drawing.Size(126, 20)
+        Me.txtMortgage.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Existing Mortgage:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtStampDuty)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtDeposit)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtCommission)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Location = New System.Drawing.Point(284, 18)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(263, 298)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Not so Inputs"
+        '
+        'txtStampDuty
+        '
+        Me.txtStampDuty.Location = New System.Drawing.Point(127, 144)
+        Me.txtStampDuty.Name = "txtStampDuty"
+        Me.txtStampDuty.ReadOnly = True
+        Me.txtStampDuty.Size = New System.Drawing.Size(126, 20)
+        Me.txtStampDuty.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 147)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Stamp Duty:"
+        '
+        'txtDeposit
+        '
+        Me.txtDeposit.Location = New System.Drawing.Point(127, 89)
+        Me.txtDeposit.Name = "txtDeposit"
+        Me.txtDeposit.ReadOnly = True
+        Me.txtDeposit.Size = New System.Drawing.Size(126, 20)
+        Me.txtDeposit.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "New Home Deposit:"
+        '
+        'txtCommission
+        '
+        Me.txtCommission.Location = New System.Drawing.Point(127, 35)
+        Me.txtCommission.Name = "txtCommission"
+        Me.txtCommission.ReadOnly = True
+        Me.txtCommission.Size = New System.Drawing.Size(126, 20)
+        Me.txtCommission.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 38)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(96, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Agent Commission:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(568, 328)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents txtNewHome As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtOldHome As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMortgage As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtStampDuty As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDeposit As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtCommission As TextBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'Calculate home loan information using the inputs.
+
+        Dim existingMortgage As Integer = Val(txtMortgage.Text)
+        Dim priceOldHome As Integer = Val(txtOldHome.Text)
+        Dim priceNewHome As Integer = Val(txtNewHome.Text)
+
+        'Output variables
+        'agents commision, deposit for New home and stamp duty
+        Dim agentCommission As Integer
+        Dim newDeposit As Integer
+        Dim StampDuty As Integer
+        Dim NewMortgage As Integer
+        Dim passvalidation As Boolean = False
+
+        If Not IsNumeric(txtMortgage.Text) Or Not IsNumeric(txtNewHome.Text) Or Not IsNumeric(txtOldHome.Text) Then
+            MsgBox("Please input a numerical value.")
+        ElseIf (Val(existingMortgage) < 0) Or (val(priceOldHome) < 0) Or (Val(priceNewHome) < 0) Then
+            MsgBox("Please input a positive value.")
+        Else
+            passvalidation = True
+        End If
+
+
+
+        If passvalidation = True Then
+
+            'calculation
+            agentCommission = priceOldHome * 2.5 / 100
+        newDeposit = priceOldHome - existingMortgage - agentCommission
+        StampDuty = 0.03 * (priceNewHome - newDeposit)
+        NewMortgage = (priceNewHome - newDeposit) + StampDuty
+
+        'Outputs
+        txtStampDuty.Text = Str(StampDuty)
+        txtCommission.Text = Str(agentCommission)
+        txtDeposit.Text = Str(newDeposit)
+
+            'Checking for government approval
+            If NewMortgage / priceNewHome < 0.8 Then
+                MsgBox("Loan Approved" + vbNewLine + vbNewLine + "New Mortgage is:" + Str(NewMortgage))
+            Else
+                MsgBox("Loan Denied")
+            End If
+
+        End If
+
+    End Sub
+End Class
